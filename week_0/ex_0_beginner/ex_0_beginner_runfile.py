47d1b4d03cb3b603d7bac7ee5689feb32010121e
MalekGUE <guedrimalek1@gmail.com>
2024-10-08 20:32:39 +0200

    I am Malek Guedri, a M1 CARe student and I would like to learn Python to visualize faster the invisible like gene expression The last time I was left without a computer was back in 2017 at my granpa's house in Algeria")

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
-import this
+import		this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n" + "I am Malek Guedri, a M1 CARe student and I would like to learn Python to visualize faster the invisible like gene expression")
 
 print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
