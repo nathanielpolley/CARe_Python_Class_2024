0d3a9345ca6a0fc4a685dc28c8c9a621ec04af1a
Rajath <bangu2001@gmail.com>
2024-10-09 19:24:17 +0530

    First Commit

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -2,4 +2,4 @@ import this
 
 print("\n"+"Hola soy Dora!! Nah not really because I have everything she has per se except for a monkey. My name is Rajath and I'm an explorer. I mostly love exploring in the ocean of knowledge. I come from the land that is after the Indus river which is profoundly known as India. Having done a Bachelor of Engineering in Biotechnology, I have landed in Toulouse for my Master's. With an interest in biology and looking at how the world is moving towards technology, I am slowly moving towards programming. I have a teensie bit of programming knowledge which I am not really confident of and that brings me here. Too long introduction. See ya soon!")
 
-print("\n"+"I'd say never. Humans are no less a computer.")
+print("\n"+"I'd say never. Humans are no less a computer in my opinion.")
