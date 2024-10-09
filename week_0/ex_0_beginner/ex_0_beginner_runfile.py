f98bdaa37ca4d89b264790e3791ed24876ed15b3
ulanbatyrova <ulanbatyrova@gmail.com>
2024-10-09 20:20:05 +0200

    Change of the text

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n"+"Hello! I'm Laila, M1 CARe student. I am concidering the prospect of moving from wet lab to dry lab that is why I want to learn Python.")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n"+"If a phone is considered as a computer, then I don't want to scare you with my answer..")
