26a56d373d538283a756e8d4c8de3ff02aaa4555
sara <scontreras3@unav.es>
2024-10-09 22:50:18 +0200

    ex_0_commands

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,19 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+# Input from the user
+import math
+
+initial_count = int(input("Enter initial count: "))
+
+# Conditional statements
+if initial_count < 1:
+    print("invalid initial count")
+
+# Again for final count
+final_count = int(input("Enter final count: "))
+
+if initial_count<1 or final_count < initial_count:
+    print("invalid final count")
+
+else:
+    calculus=math.log(final_count) - math.log(initial_count)
+    print(f"Growth rate is: {calculus}")
