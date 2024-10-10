1823b0ea218d9bc57c614cf3c08e77813809a2ad
MalekGUE <guedrimalek1@gmail.com>
2024-10-10 18:06:07 +0200

    the submit file was changed

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
-import		this
+import	this
 
 print("\n" + "I am Malek Guedri, a M1 CARe student and I would like to learn Python to visualize faster the invisible like gene expression")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n" + "The last time I was left without a computer was back at my grandpa's house in 2017")
