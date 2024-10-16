b86ec7dd1182eb8d2a0f4ef9608e02e8853780db
Nadine <nadineeadham@gmail.com>
2024-10-17 00:15:04 +0200

    Enter initial count: 10 Enter final count: 20 Enter time elapsed: 2 Growth rate: 5.0
    Process finished with exit code 0

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,9 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+
+initial_count = float(input("Enter initial count: "))
+final_count = float(input("Enter final count: "))
+time = float(input("Enter time elapsed: "))
+
+growth_rate = (final_count - initial_count) / time
+
+print("Growth rate:", growth_rate)
