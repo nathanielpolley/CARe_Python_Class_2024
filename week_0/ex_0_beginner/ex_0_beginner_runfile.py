9211a5dc8b85aba8c7d7f642469a67795c1ffecd
laurajouvin <jouvinlaura@gmail.com>
2024-10-27 16:51:33 +0100

    Beginners' assignement complete

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n"+"My name is Laura, I am doing a PhD on modelising Oncolityc virus infection which requires python which is why I want to learn python")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n"+"I can't remember the last type i did,'t have my phone or any kind of computer")
