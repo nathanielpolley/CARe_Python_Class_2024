33552632343ac20c29e826e3b2d5a345f5ea9a0e
Nadine406 <nadineeadham@gmail.com>
2024-10-27 23:28:16 +0100

    Update ex_1_beginner_runfile.py

--- week_1/ex_1_beginner/ex_1_beginner_runfile.py
+++ week_1/ex_1_beginner/ex_1_beginner_runfile.py
@@ -1,1 +1,23 @@
 #YOUR CODE FOR EX_1 BEGINNER HERE
+
+population_counts = []
+
+for i in range(5):
+    count = int(input(f"Enter population count for day {i + 1}: "))
+    population_counts.append(count)
+
+average_population = sum(population_counts) / len(population_counts)
+max_population = max(population_counts)
+min_population = min(population_counts)
+
+print("Average Population:", average_population)
+print("Maximum Population:", max_population)
+print("Minimum Population:", min_population)
+
+x = 200
+print(f"Days with population count exceeding {x}:")
+for day, count in enumerate(population_counts, start=1):
+    if count > x:
+        print("The day corresponds to Day", day, "and the value is", count)
+else :
+    print ("No population exceeding 200")
