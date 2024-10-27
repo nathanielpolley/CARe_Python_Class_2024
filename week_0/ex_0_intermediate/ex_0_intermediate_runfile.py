9de67e8e907c17bf30e23ae5680d978c9de97f6a
ASLIOZKUL <asli.ozkul@univ-tlse3.fr>
2024-10-27 20:34:05 +0100

    Update ex_0_intermediate_runfile.py
    Updated Version

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -3,12 +3,15 @@ import math
 user_input_initial_count = input("Enter the initial cell count: ")
 user_input_final_count= input("Enter the final cell count:")
 user_input_time = input("Enter the time elapsed (in hours): ")
-if user_input_initial_count and user_input_final_count and user_input_time.isdigit():
-    cell_count=int(user_input_initial_count and user_input_final_count and user_input_time)
-    print(f"initial_cell:{user_input_initial_count},final_cell:{user_input_final_count}, elapsed_time:{user_input_time}")
-
-if int(user_input_final_count) > int(user_input_initial_count) :
-   growth_rate = (math.log(int(user_input_final_count)) - math.log(int(user_input_initial_count))) / int(user_input_time)
-   print(f"growth_rate: {growth_rate}")
+if user_input_initial_count.isdigit() and user_input_final_count.isdigit() and user_input_time.isdigit():
+    initial_count = int(user_input_initial_count)
+    final_count = int(user_input_final_count)
+    time_elapsed = int(user_input_time)
+    if final_count>initial_count:
+         growth_rate = (math.log(int(final_count)) - math.log(int(initial_count))) / int(time_elapsed)
+         print(f"growth_rate: {growth_rate} per hour")
+    else:
+        print('Error in the experiment')
 else:
-      print('Repeat the experiment')
+  print('ERROR')
+
