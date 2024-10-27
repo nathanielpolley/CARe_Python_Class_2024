36d632115858d079756b1fba36df4d567a14d8f6
laurajouvin <jouvinlaura@gmail.com>
2024-10-27 17:35:10 +0100

    Beginners' assignement complete

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,9 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+initial_count = int(input("Enter your initial count: "))
+final_count = int(input("Enter your final count: "))
+time = int(input("Enter your time: "))
+if time <= 0:
+    time = int(input("Time invalid, enter a new time bigger than 0: "))
+
+print((math.log(final_count) - math.log(initial_count)) / time)
