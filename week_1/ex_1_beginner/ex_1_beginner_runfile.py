870fd556b332e232aa93b4c17181bbf6c706852f
ulanbatyrova <ulanbatyrova@gmail.com>
2024-11-01 16:25:31 +0100

    1 week assignments

--- week_1/ex_1_beginner/ex_1_beginner_runfile.py
+++ week_1/ex_1_beginner/ex_1_beginner_runfile.py
@@ -1,1 +1,8 @@
-#YOUR CODE FOR EX_1 BEGINNER HERE
+population_counts = [100, 200, 150, 300, 50]
+average = int(sum(population_counts) / len(population_counts))
+max = max(population_counts)
+min = min(population_counts)
+print(f'Average population count: {average}\nMaximum population count: {max}\nMinimum population count: {min}')
+for days, count in enumerate(population_counts, start=1):
+    if count > 200:
+        print(f'Days number when the population exceeds 200: {days}')
--- week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
+++ week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
@@ -1,1 +1,35 @@
-#YOUR CODE FOR EX_1 INTERMEDIATE HERE
+species_dict = {}
+while True:
+    species_name = input('Enter species name (or type "exit" to quit): ')
+    if species_name.lower() == 'exit':
+        break
+    try:
+        num_samples = int(input(f'Enter number of samples for {species_name}: '))
+    except ValueError:
+        print("Invalid input. Please enter a valid number for samples.")
+        continue
+    species_dict[species_name] = num_samples
+total_samples = 0
+for species, samples in species_dict.items():
+    total_samples += samples
+print(f'Number of total samples: {total_samples}')
+species_15 = []
+for species, samples in species_dict.items():
+    if samples > 15:
+        species_15.append(species)
+if species_15:
+    print(f'Microbial species with sample counts greater than 15: {', '.join(species_15)}')
+else:
+    print('No microbial species have sample counts greater than 15.')
+def add_new_species(new_species, species_dict):
+    if new_species in species_dict:
+        species_dict[new_species] += 1
+    else:
+        species_dict[new_species] = 1
+while True:
+    new_species = input('Enter a new species name (or type "exit" to quit): ')
+    add_new_species(new_species, species_dict)
+    if new_species.lower() == 'exit':
+        break
+species_string = ', '.join(f'{key}: {value}' for key, value in species_dict.items())
+print(f'Updated species count: {species_string}')
