1c0796dfc45b840d84da213cd219a660fe1eaf9e
Zkassem17 <zeina-kassem2011@hotmail.com>
2024-11-01 22:18:56 +0100

    Add round to calculation

--- week_0/ex_0_intermediate/Intermediate week 0.py
+++ week_0/ex_0_intermediate/Intermediate week 0.py
@@ -1,17 +1,18 @@
 import math
 final=input("enter final count=")
 init= input("enter initial input=")
-Time= input("enter time input=")
-if final.isdigit() and init.isdigit() and Time.isdigit():
+time= input("enter time input=")
+
+if final.isdigit() and init.isdigit() and time.isdigit():
     final=int(final)
     init=int(init)
-    time=int(Time)
+    time=int(time)
 
     if final >init:
        calculation= (math.log(final)-math.log(init))/time
-       print(f"cell growth rate is: {calculation}")
+       print(f"cell growth rate is: {round(calculation,4)}")
     else:
-       print("please input final count greater than initial")
+        print("please input final count greater than initial")
 else:
       print("please use valid numerical input (no negatives or decimals")
 
