09176de8aee033efbeafccaa472469a1f5d14c19
ASLIOZKUL <asli.ozkul@univ-tlse3.fr>
2024-11-15 22:00:47 +0100

    Update ex_1_beginner_runfile.py
    week1_beginner_assignment

--- week_1/ex_1_beginner/ex_1_beginner_runfile.py
+++ week_1/ex_1_beginner/ex_1_beginner_runfile.py
@@ -1,1 +1,24 @@
 #YOUR CODE FOR EX_1 BEGINNER HERE
+microbial_population_counts=[100, 200, 150, 300, 50]
+from statistics import mean
+
+def average_population(microbial_population_counts):
+    return mean(microbial_population_counts)
+avg_population = average_population(microbial_population_counts)
+print(f"The average population is: {avg_population}")
+
+max_count = min_count = microbial_population_counts[0]
+
+for count in microbial_population_counts:
+    if count > max_count:
+        max_count = count
+    if count < min_count:
+        min_count = count
+
+print(f"Maximum population count: {max_count}")
+print(f"Minimum population count: {min_count}")
+
+
+for day, count in enumerate(microbial_population_counts, start=1):
+    if count > 200:
+        print(f"Day {day}: {count}")
